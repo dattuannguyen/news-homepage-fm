2649e1aaa501ffaaaf0e0474ab8d26373ec13e26
dattuannguyen <dat.nt0609@gmail.com>
2024-02-13 16:19:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector('.sidebar');
+
+const showSideBar =() =>{
+    sidebar.style.display='flex';
+}
+
+const closeSideBar=()=>{
+    sidebar.style.display='none';
+}
